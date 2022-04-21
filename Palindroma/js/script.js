9adf4bf247442771bec20252246fca4e41219a08
comemaino <mat.maino@gmail.com>
2022-04-21 15:32:39 +0200

    Removed: for from Palindroma script

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -6,11 +6,10 @@ checkIfPalindrome(userWordArray);
 
 function checkIfPalindrome(arrayToCheck) {
   let isPalindrome = false;
-  for (let i = 0; i < arrayToCheck.length; i++) {
-    if (arrayToCheck === arrayToCheck.reverse()) {
-      isPalindrome = true;
-    }
+  if (arrayToCheck === arrayToCheck.reverse()) {
+    isPalindrome = true;
   }
+
   console.log(isPalindrome);
   return isPalindrome;
 }
