760e4e164f63acad1d1d1656ed8db16f27b12256
comemaino <mat.maino@gmail.com>
2022-04-21 15:15:37 +0200

    Add: Palindroma script

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,1 +1,16 @@
+const userWord = prompt("dimmi una parola");
+const userWordArray = userWord.split("");
 
+console.log(userWordArray);
+checkIfPalindrome(userWordArray);
+
+function checkIfPalindrome(arrayToCheck) {
+  let isPalindrome = false;
+  for (let i = 0; i < arrayToCheck.length; i++) {
+    if (arrayToCheck === arrayToCheck.reverse()) {
+      isPalindrome = true;
+    }
+  }
+  console.log(isPalindrome);
+  return isPalindrome;
+}
